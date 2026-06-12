c4b445a7f00718292c5f8e74f376a8502455ee2e
Moliang Zhou <endeavourwilliam.zhou@gmail.com>
2026-06-12 16:08:59 -0700

    chore: setup custom domain moliang.ai

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,11 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     output: 'export',
-    basePath: isProd ? '/portfolio-showcase' : '',
     images: {
         unoptimized: true
     },
     env: {
-        NEXT_PUBLIC_BASE_PATH: isProd ? '/portfolio-showcase' : '',
+        NEXT_PUBLIC_BASE_PATH: '',
     }
 };
 
